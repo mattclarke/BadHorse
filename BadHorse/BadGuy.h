8f9cae7f19db1a39dd8509eb2a36166d9c8693e9
Matt Clarke <matt.clarke@stfc.ac.uk>
2016-12-13 12:00:24 +0000

    Split out into header file

---
+++ BadHorse/BadGuy.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <set>
+
+
+class BadGuy {
+private:
+	std::set<std::shared_ptr<BadGuy>> enemies;
+
+public:
+	static void addEnemyAndViceVersa(std::shared_ptr<BadGuy> enemy1, std::shared_ptr<BadGuy> enemy2);
+
+	size_t getNumberEnemies() const {
+		return enemies.size();
+	}
+
+	void addEnemy(std::shared_ptr<BadGuy> enemy) {
+		enemies.insert(enemy);
+	}
+
+	bool isEnemy(std::shared_ptr<BadGuy> enemy) {
+		auto it = enemies.find(enemy);
+		if (it != enemies.end()) {
+			return true;
+		}
+
+		return false;
+	}
+};
+
+inline void BadGuy::addEnemyAndViceVersa(std::shared_ptr<BadGuy> enemy1, std::shared_ptr<BadGuy> enemy2) {
+	enemy1->addEnemy(enemy2);
+	enemy2->addEnemy(enemy1);
+}
--- BadHorse/TestMain.cpp
+++ BadHorse/TestMain.cpp
@@ -1,38 +1,7 @@
-#include <iostream>
 #include <vector>
 #include <memory>
 #include <set>
-
-
-class BadGuy {
-private:
-	std::set<std::shared_ptr<BadGuy>> enemies;
-
-public:
-	static void addEnemyAndViceVersa(std::shared_ptr<BadGuy> enemy1, std::shared_ptr<BadGuy> enemy2);
-
-	size_t getNumberEnemies() {
-		return enemies.size();
-	}
-
-	void addEnemy(std::shared_ptr<BadGuy> enemy) {
-		enemies.insert(enemy);
-	}
-
-	bool isEnemy(std::shared_ptr<BadGuy> enemy) {
-		auto it = enemies.find(enemy);
-		if (it != enemies.end()) {
-			return true;
-		}
-
-		return false;
-	}
-};
-
-void BadGuy::addEnemyAndViceVersa(std::shared_ptr<BadGuy> enemy1, std::shared_ptr<BadGuy> enemy2) {
-	enemy1->addEnemy(enemy2);
-	enemy2->addEnemy(enemy1);
-}
+#include "BadGuy.h"
 
 
 class Graph {
@@ -40,7 +9,7 @@ private:
 	std::set<std::shared_ptr<BadGuy>> nodes;
 
 public:
-	int getNumberNodes() {
+	int getNumberNodes() const {
 		return nodes.size();
 	}
 
@@ -55,7 +24,7 @@ public:
 
 class BadGuyTest : public ::testing::Test {
 protected:
-	virtual void SetUp() {
+	virtual void SetUp() override {
 		baddie = std::make_shared<BadGuy>();
 		enemy = std::make_shared<BadGuy>();
 		BadGuy::addEnemyAndViceVersa(baddie, enemy);
